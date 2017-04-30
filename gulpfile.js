d3d0de61cb0093d80eb89d60b0d64f999ab3a27b
tetsumote <unionproject88@gmail.com>
2017-05-01 00:12:31 +0200

    another changes in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp'),
 gutil = require('gulp-util'),
 coffee = require('gulp-coffee');
 
+var coffeeSource = ['components/coffee/tagline.coffee'];
+
 gulp.task('coffee',function(){
-    gulp.src('components/coffee/tagline.coffee')
+    gulp.src(coffeeSource)
     .pipe(coffee({bare:true})
      .on('error',gutil.log))
      .pipe(gulp.dest('components/scripts'))
